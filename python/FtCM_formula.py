e0454a3b777470d117d96232fa6906c6c1d29f08
Pablo De Castro Manzano <pablodecm@gmail.com>
2015-06-12 13:03:51 -0400

    Generic prob. formula generation working

--- python/FtCM_formula.py
+++ python/FtCM_formula.py
@@ -1,19 +1,36 @@
+from sympy import Symbol, IndexedBase, Product
+from sympy import binomial
 
-def multisets( n, k ):
+def multiset( n, k ):
     if k == 0: return [(0,)*n]
     if n == 0: return []
     if n == 1: return [(k,)]
-    return [(0,)+val for val in multisets(n-1,k)] + \
-            [(val[0]+1,)+val[1:] for val in multisets(n,k-1)]
+    return [(0,)+val for val in multiset(n-1,k)] + \
+            [(val[0]+1,)+val[1:] for val in multiset(n,k-1)]
 
-def submultisets( m, k ):        
-    msets = multisets( len(m),k)
-    smsets = []
-    for mset in msets: 
-        if (all((e_s <= e_m) for e_s, e_m in zip(mset, m))):
-            smsets.append(mset)
-    return smsets        
+def submultiset( m, k ):        
+    mset = multiset( len(m),k)
+    smset = []
+    for comp in mset: 
+        if (all((e_c <= e_m) for e_c, e_m in zip(comp, m))):
+            smset.append(comp)
+    return smset        
 
+def prob_component( comp, m_comp):
+    n_t = len(comp) # number of types
+    t = Symbol('t') # type of jet
+    C = IndexedBase('C') # componenent number vector 
+    N = IndexedBase('N') # max componenent number vector
+    E = IndexedBase('E') # efficiency vector 
+    product =  Product(binomial(N[t],C[t])*E[t]**(C[t])*(1-E[t])**(N[t]-C[t]), (t,0,len(comp)-1))
+    C_dict = { C[i] : comp[i] for i in range(n_t) }
+    N_dict = { N[i] : m_comp[i] for i in range(n_t) }
+    subs_dict = dict(C_dict.items() + N_dict.items())
+    return product.doit().subs(subs_dict)
 
+def prob_submultiset( m, k):
+    smset = submultiset(m, k) # get possible permutations
+    comp_list = [prob_component(comp, m) for comp in smset] 
+    return sum(comp_list) # sum the prob of all components 
 
         
--- test/test_FtCM_formula.py
+++ test/test_FtCM_formula.py
@@ -1,8 +1,17 @@
 
-from mut_framework.BTagSF_FtCM.FtCM_formula import multisets, submultisets
+from mut_framework.BTagSF_FtCM.FtCM_formula import multiset, submultiset
+from mut_framework.BTagSF_FtCM.FtCM_formula import prob_component, prob_submultiset
 
-print "multisets(1,2) = " + str(multisets(1,2))
-print "multisets(2,3) = " + str(multisets(2,3))
-print "multisets(3,3) = " + str(multisets(3,3))
-print "submultisets((1,1,1),3) = " + str(submultisets((1,1,1),3))
-print "submultisets((3,3,3),3) = " + str(submultisets((3,3,3),3))
+print "multiset(1,2) = " + str(multiset(1,2))
+print "multiset(2,3) = " + str(multiset(2,3))
+print "multiset(3,3) = " + str(multiset(3,3))
+print "multiset(3,1) = " + str(multiset(3,1))
+print "submultiset((1,1,1),3) = " + str(submultiset((1,1,1),3))
+print "submultiset((3,3,3),3) = " + str(submultiset((3,3,3),3))
+print "submultiset((1,1,2),1) = " + str(submultiset((1,1,2),1))
+
+print "prob_component((1,0,0),(1,1,2)) = " +str(prob_component((1,0,0),(1,1,2))) 
+print "prob_component((0,1,0),(1,1,2)) = " +str(prob_component((0,1,0),(1,1,2))) 
+print "prob_component((0,0,1),(1,1,2)) = " +str(prob_component((0,0,1),(1,1,2))) 
+print "prob_submultiset((1,1,2), 1) = " +str(prob_submultiset((1,1,2), 1)) 
+print "prob_submultiset((1,1,2), 1).expand() = " + str(prob_submultiset((1,1,2), 1).expand()) 
