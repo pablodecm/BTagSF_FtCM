87e23b97a726d3a55428196b79c8e2ac57343f8f
Pablo De Castro Manzano <pablodecm@gmail.com>
2015-06-18 10:01:45 -0400

    Change name to Sample and add new DataManager

--- python/DataManager.py
+++ python/DataManager.py
@@ -1,9 +1,10 @@
 from ast import literal_eval
 import json
 from mut_framework.BTagSF_FtCM.FtCM_formula import formula_categories
-from sympy import Symbol
+from sympy import IndexedBase, Symbol, sympify
+import pickle
 
-class DataManager:
+class Sample:
     
     def __init__(self, filename, s_name,  nEventGen, xSec): 
         with open(filename) as f:
@@ -14,6 +15,8 @@ class DataManager:
         self.ptBins = j["ptBins"]
         self.etaBins = j["etaBins"]
         self.nEventGen = nEventGen
+        self.good_cat_jets = j["good_cat_jets"] 
+        self.tag_cat_jet = j["tag_cat_jets"]
         self.xSec = xSec
         self.nEventPass = j["nEventPass"]
         self.tagMultiplicity = j["tagMultiplicity"]
@@ -35,6 +38,38 @@ class DataManager:
         return factor*formula_categories(self.cat_fractions(), n_tags)
 
         
+class DataManager:
+
+    def __init__( self, samples, signal = "TTbar" ):
+        self.samples = samples 
+        self.signal = signal 
+        self.nCat = len(samples[0].cat_counts.keys()[0])
+
+    def full_formula(self, n_tags):    
+        f = sympify(0)
+        for s in self.samples:
+            if s.s_name is self.signal:
+                f += s.formula(n_tags)
+            else:
+                kappa = Symbol("kappa")
+                f += kappa*s.formula(n_tags).subs(Symbol("xsec_{}".format(s.s_name)),s.xSec)
+        return f
+
+    def subs_formula(self, n_tags, extra_subs = {}):
+        E = IndexedBase("E")
+        subs_dict = {E[0] : Symbol("eff_x"),
+                     E[1] : Symbol("eff_l"),
+                     E[2] : Symbol("eff_c")}  
+        b_eff_dict ={ E[i] : Symbol("eff_b{}".format(i-3)) for i in range(3,self.nCat)} 
+        subs_dict = dict(subs_dict.items() + b_eff_dict.items() + extra_subs.items())
+        return self.full_formula(n_tags).subs(subs_dict)
+
+    def save_formula_list( self, filename, max_n_tags =7):
+        f_list = []
+        for n_tags in range(max_n_tags):
+            f_list.append(self.subs_formula(n_tags))
+        with open(filename,'w') as f:
+            pickle.dump(f_list, f)
 
 
 
--- test/test_DataManager.py
+++ test/test_DataManager.py
@@ -1,7 +1,7 @@
 
-from mut_framework.BTagSF_FtCM.DataManager import DataManager
+from mut_framework.BTagSF_FtCM.DataManager import Sample, DataManager
 
-ttbar = DataManager("../output/TTbar_Summer13.json", "TTbar", 6923750, 240.0)
+ttbar = Sample("../output/TTbar_Summer13.json", "TTbar", 6923750, 240.0)
 
 print "PreTagging Efficiency: " + str(ttbar.pretag_eff())
 tagger = "combinedMVABJetTags"
@@ -12,4 +12,18 @@ print "catFractions: " + str(cat_fractions)
 print "sum catFractions: " + str(sum(cat_fractions.values()))
 print "ttbar formula 0 btag: " + str(ttbar.formula(0))
 print "ttbar formula 1 btag: " + str(ttbar.formula(1))
+
+
+samples = [Sample("../output/TTbar_Summer13.json", "TTbar", 6923750, 240.0),
+           Sample("../output/WJets_Summer13.json", "WJets", 76041475, 36257.2),
+           Sample("../output/T_tW_Summer13.json", "T_tW", 497658, 11.15),
+           Sample("../output/Tbar_tW_Summer13.json", "Tbar_tW", 493460, 11.15),
+           Sample("../output/Tbar_t_Summer13.json", "Tbar_t", 1935072, 30.0),
+           Sample("../output/ZJets_Summer13.json", "ZJets", 30459503, 3503.0),
+           Sample("../output/T_t_Summer13.json", "T_t", 3758227, 55.531)]
+
+lumi = 19789.0
+
+dm = DataManager(samples)
+dm.save_formula_list("../output/f_list.pickle")
 
